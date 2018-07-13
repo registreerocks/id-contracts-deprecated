0aec24f9289669a16bc72fd21cfea9a9a95f6cb1
sabinebertram <sabine.bertram@mailbox.org>
2018-07-13 22:20:27 +0200

    update for deployment on server

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
           host: "localhost",
           port: 7545,
           network_id: "*" // Match any network id
-      }
+      },
+      ganache: {
+        host: "206.189.4.230",
+        port: 8545,
+        network_id: "*" // Match any network id
+    }
   }
 };
