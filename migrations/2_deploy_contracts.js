6a199e11b20435a14f66d7131e0261e3999b8db1
sabinebertram <sabine.bertram@mailbox.org>
2018-07-13 22:20:27 +0200

    update for deployment on server

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,14 +2,18 @@ var Ownable = artifacts.require("./Ownable.sol");
 var IdContract = artifacts.require("./IdContract.sol");
 var SearchContract = artifacts.require("./SearchContract.sol");
 
+// module.exports = function(deployer) {
+//     deployer.then(function() {
+//         return deployer.deploy(Ownable);
+//     }).then(function() {
+//         return deployer.deploy(SearchContract);
+//     }).then(function() {
+//         return SearchContract.deployed();
+//     }).then(function(instance) {
+//         return deployer.deploy(IdContract, "001", instance.address); // "001" for testing
+//     });
+// };
+
 module.exports = function(deployer) {
-    deployer.then(function() {
-        return deployer.deploy(Ownable);
-    }).then(function() {
-        return deployer.deploy(SearchContract);
-    }).then(function() {
-        return SearchContract.deployed();
-    }).then(function(instance) {
-        return deployer.deploy(IdContract, "001", instance.address); // "001" for testing
-    });
+    deployer.deploy(SearchContract);
 };
