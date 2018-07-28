940a9dea1f6441233911f0db171870b14ece9f9c
sabinebertram <sabine.bertram@mailbox.org>
2018-07-28 12:56:03 +0200

    get identifying ids and bulk get

--- package/src/swagger_server/controllers/id_contract_API_functions.py
+++ package/src/swagger_server/controllers/id_contract_API_functions.py
@@ -1,6 +1,7 @@
 from .transaction_functions import (_delete_id, _deploy_id_contract,
-                                    _get_id_contract, _set_id,
-                                    _set_id_after_deletion)
+                                    _get_id_contract_address,
+                                    _get_identifying_id, _get_identifying_ids,
+                                    _set_id, _set_id_after_deletion)
 
 
 def deploy_id_contract(body):
@@ -16,4 +17,10 @@ def set_id_after_deletion(body):
     return _set_id_after_deletion(body.get('address'), body.get('id'))
 
 def get_id_contract_address(id):
-    return _get_id_contract(id)
+    return _get_id_contract_address(id)
+
+def get_identifying_id(address):
+    return _get_identifying_id(address)
+
+def get_identifying_ids(addresses):
+    return _get_identifying_ids(addresses)
--- package/src/swagger_server/controllers/transaction_functions.py
+++ package/src/swagger_server/controllers/transaction_functions.py
@@ -15,7 +15,7 @@ def _deploy_id_contract(_id):
     return tx_receipt.contractAddress
 
 def _set_id(_id, new_id):
-    id_contract = _get_id_contract(_id)
+    id_contract = W3.eth.contract(address=_get_id_contract_address(_id), abi=id_contract_interface['abi'])
     try:
         id_contract.functions.deleteId(SEARCH_CONTRACT_ADDRESS).transact({'from': W3.eth.accounts[0]})
         id_contract.functions.setId(new_id, SEARCH_CONTRACT_ADDRESS).transact({'from': W3.eth.accounts[0]})
@@ -24,7 +24,7 @@ def _set_id(_id, new_id):
         return {'ERROR': 'No contract corresponding to this id found.'}, 400
 
 def _delete_id(_id):
-    id_contract = _get_id_contract(_id)
+    id_contract = W3.eth.contract(address=_get_id_contract_address(_id), abi=id_contract_interface['abi'])
     try:
         id_contract.functions.deleteId(SEARCH_CONTRACT_ADDRESS).transact({'from': W3.eth.accounts[0]})
         return True
@@ -39,9 +39,20 @@ def _set_id_after_deletion(address, new_id):
     except:
         return {'ERROR': 'No contract corresponding to this address found.'}, 400
 
-def _get_id_contract(_id):
+def _get_id_contract_address(_id):
     search_contract = W3.eth.contract(address=SEARCH_CONTRACT_ADDRESS, abi=search_contract_interface['abi'])
     keccak_hash = keccak.new(digest_bits=256)
     keccak_hash.update(_id.encode())
     id_contract_address = search_contract.functions.idcontracts(keccak_hash.hexdigest()).call()
     return id_contract_address
+
+def _get_identifying_id(address):
+    id_contract = W3.eth.contract(address=address, abi=id_contract_interface['abi'])
+    _id = id_contract.functions.id().call()
+    return _id
+
+def _get_identifying_ids(addresses):
+    _ids = dict()
+    for address in addresses:
+        _ids[address] = _get_identifying_id(address)
+    return _ids
