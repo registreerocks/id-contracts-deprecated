c3d68bd9813e5370223013217f5183e81da92cb7
sabinebertram <sabine.bertram@mailbox.org>
2018-09-06 14:08:28 +0200

    adds access control

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,36 @@
 FROM python:3.6
 
+#SSL
+# RUN openssl req \
+#     -new \
+#     -newkey rsa:4096 \
+#     -days 365 \
+#     -nodes \
+#     -x509 \
+#     -subj "/C=ZA/ST=WC/L=CapeTown/O=Registree/CN=www.registree.rocks" \
+#     -keyout server.key \
+#     -out server.cert
+
+# connexion
+RUN mkdir -p /usr/src
+COPY oas3.zip /usr/src
+WORKDIR /usr/src
+
+RUN  apt-get update -y && \
+     apt-get upgrade -y && \
+     apt-get install unzip -y 
+
+RUN unzip oas3.zip
+RUN mv connexion-oas3 connexion
+WORKDIR connexion
+RUN pip install -e .
+
+# API
 RUN mkdir -p /usr/src/app
-WORKDIR /usr/src/app
-
 COPY ./package /usr/src/app
-
 RUN mkdir /usr/src/app/contracts
 COPY ./build/contracts /usr/src/app/contracts
-
+WORKDIR /usr/src/app
 RUN pip3 install -e .
 
 EXPOSE 8080
--- package/src/swagger_server/controllers/id_contract_API_functions.py
+++ package/src/swagger_server/controllers/id_contract_API_functions.py
@@ -1,3 +1,4 @@
+from .authentication import requires_auth, requires_scope
 from .transaction_functions import (_claim_ownership, _delete_id,
                                     _deploy_id_contract,
                                     _deregister_allowed_user,
@@ -6,33 +7,52 @@ from .transaction_functions import (_claim_ownership, _delete_id,
                                     _register_allowed_user, _set_id,
                                     _set_id_after_deletion)
 
-
+@requires_auth
+@requires_scope('admin')
 def deploy_id_contract(body):
     return _deploy_id_contract(body.get('id'), body.get('db_url'))
 
+@requires_auth
+@requires_scope('student')
 def set_id(body):
     return _set_id(body.get('current_id'), body.get('new_id'))
 
+@requires_auth
+@requires_scope('student')
 def delete_id(body):
     return _delete_id(body.get('id'))
 
+@requires_auth
+@requires_scope('student')
 def set_id_after_deletion(body):
     return _set_id_after_deletion(body.get('address'), body.get('id'))
 
+@requires_auth
+@requires_scope('admin', 'lecturer', 'student')
 def get_id_contract_address(id):
     return _get_id_contract_address(id)
 
+@requires_auth
+@requires_scope('admin', 'lecturer', 'student')
 def get_identifying_id(address):
     return _get_identifying_id(address)
 
+@requires_auth
+@requires_scope('admin', 'lecturer')
 def get_identifying_ids(body):
     return _get_identifying_ids(body.get('addresses'))
 
+@requires_auth
+@requires_scope('student')
 def claim_ownership(body):
     return _claim_ownership(body.get('contract_address'), body.get('new_owner'))
 
+@requires_auth
+@requires_scope('admin', 'student')
 def register_allowed_user(body):
     return _register_allowed_user(body.get('contract_address'), body.get('user_address'))
 
+@requires_auth
+@requires_scope('admin', 'student')
 def deregister_allowed_user(body):
     return _deregister_allowed_user(body.get('contract_address'), body.get('user_address'))
