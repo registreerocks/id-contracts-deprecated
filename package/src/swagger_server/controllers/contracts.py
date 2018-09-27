1d18a9679ee315ab996be2f726108d49d8b17c3c
SoIidarity <christopher.maree@gmail.com>
2018-09-27 18:20:06 +0200

    updated nginx config and changed to new port

--- package/src/swagger_server/controllers/contracts.py
+++ package/src/swagger_server/controllers/contracts.py
@@ -1,7 +1,7 @@
 import json
 
-with open('contracts/IdContract.json', 'r') as f:
+with open('../contracts/IdContract.json', 'r') as f:
     id_contract_interface = json.load(f)
 
-with open('contracts/SearchContract.json', 'r') as f:
+with open('../contracts/SearchContract.json', 'r') as f:
     search_contract_interface = json.load(f)
